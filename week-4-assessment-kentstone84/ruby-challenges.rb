95cb171a06d15bbed7cedc06fa5543832f0757cd
Kent Stone <69402437+kentstone84@users.noreply.github.com>
2023-01-11 14:28:43 -0500

    Add files via upload
    Uploaded Assessments

---
+++ week-4-assessment-kentstone84/ruby-challenges.rb
@@ -0,0 +1,72 @@
+# ASSESSMENT 4: Ruby Coding Practical Questions
+# MINASWAN ✌️
+
+# --------------------1) Create a method that takes in a number and determines if the number is even or odd. Use the test variables provided.
+
+num1 = 7
+# Expected output: '7 is odd'
+num2 = 42
+# Expected output: '42 is even'
+num3 = 221
+# Expected output: '221 is odd'
+
+def even_odd num
+    if num % 2 == 0
+        return "#{num} is even"
+    else
+        return "#{num} is odd"
+    end
+end
+
+p even_odd(num1)
+p even_odd(num2)
+p even_odd(num3)
+
+result: '7 is odd' '42 is even' '221 is odd'
+
+
+
+# -------------------2) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. 
+# HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
+
+beatles_album1 = 'Rubber Soul'
+# Expected output: 'Rbbr Sl'
+beatles_album2 = 'Sgt Pepper'
+# Expected output: 'Sgt Pppr'
+beatles_album3 = 'Abbey Road'
+# Expected output: 'bby Rd'
+
+def no_vowels string
+    string.delete("aeiouAEIOU")
+end
+
+p no_vowels beatles_album1
+p no_vowels beatles_album2
+p no_vowels beatles_album3
+
+Result: 'Rbbr Sl' 'Sgt Pppr' 'bby Rd'
+
+
+# -------------------3) Create a method that takes in a string and checks if the string is a palindrome. A palindrome is the same word spelled forward or backward. Use the test variables provided.
+
+palindrome_tester1 = 'Racecar'
+# Expected output: 'Racecar is a palindrome'
+palindrome_tester2 = 'LEARN'
+# Expected output: 'LEARN is not a palindrome'
+palindrome_tester3 = 'Rotator'
+# Expected output: 'Rotator is a palindrome'
+
+
+def palindrome string
+    if string == string.reverse
+        return "#{string} is a palindrome"
+    else
+        return "#{string} is not a palindrome"
+    end
+end
+
+p palindrome palindrome_tester1
+p palindrome palindrome_tester2
+p palindrome palindrome_tester3
+
+Result: 'Racecar is a palindrome' 'LEARN is not a palindrome' 'Rotator is a palindrome'
